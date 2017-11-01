2c421338ab0234237ce7d67c3ea796cfe3643931
sfu2000 <sfu2000@gmail.com>
2017-11-01 15:54:46 -0400

    Cleaned up code

--- grades.py
+++ grades.py
@@ -1,33 +1,46 @@
+def stemPlot(gradeList):
+    stem = 9
+    for x in reversed(range(stem)):
+        line = str(x) + ' | '
+        for grade in gradeList:
+            if int(str(grade)[0]) == x:
+                line += str(grade)[1:] + " "
+        if line != str(x) + " | ":
+            print(line)
+
 with open("grades.txt") as f:
     content = f.readlines()
 
 content = [x.strip() for x in content]
 content.pop(0)
-gradeList = [(int(x[0:2])+int(x[5:7])+2*int(x[-2:])) for x in content]
-gradeList = [float(x)/400 for x in gradeList]
-gradeList = [x*100 for x in gradeList]
+content = [[x.split()][0] for x in content]
+contentFloat = [[float(x) for x in y] for y in content]
 
+finalGradeList = [int((contentFloat[x][0]+contentFloat[x][1]+2*contentFloat[x][2])/400*100) for x in range(len(contentFloat))]
+midtermGradeList = [int(contentFloat[x][2]) for x in range(len(contentFloat))]
 import numpy as np
-np.std(gradeList)
-np.mean(gradeList)
-
-stem = 8
-
-for x in gradeList:
-    first = int(str(x)[0])
-    if first == stem:
-        line = ' | '
-        for y in gradeList:
-            if str(y)[0] == str(first):
-                line += str(int(y))[1:] + ' '
-        print(str(stem) + line)
-        stem -= 1
+print("Mean: "+str(np.mean(finalGradeList)))
+print("Standard deviation: "+str(np.std(finalGradeList)))
+print("Final Grade Plot")
+stemPlot(finalGradeList)
+print("Midterm Grade Plot")
+stemPlot(midtermGradeList)
 
 '''
-Result =
-8 | 8 7 6 6 3 1 0 0 0 0 0
-7 | 8 7 7 6 4 4 3 3 3 3 2 2 1 1 0 0
-6 | 9 8 7 6 6 6 4 3 3 1
-5 | 9 7 7 6 3
-4 | 3
+Mean: 70.1136363636
+Standard deviation: 12.102246052
+Final Grade Plot
+8 | 8 7 6 6 3 1 0 0 0 0 0 
+7 | 8 7 7 6 4 4 3 3 3 3 2 2 1 1 0 0 
+6 | 9 8 7 6 6 6 4 3 3 1 
+5 | 9 7 7 6 3 
+4 | 3 
+2 | 2 
+Midterm Grade Plot
+8 | 1 0 1 
+7 | 8 0 7 1 2 4 4 7 0 2 3 
+6 | 9 6 3 1 8 9 7 2 7 5 7 0 6 1 6 
+5 | 9 6 2 2 2 0 
+4 | 5 5 9 5 6 7 7 3 
+3 | 8 
 '''
